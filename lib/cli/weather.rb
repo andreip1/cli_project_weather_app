2b089a46d22db297d8e1b49abf64bf061ec37429
Swishyfishie <pclandreiuk@gmail.com>
2020-02-05 22:24:30 +0000

    added attraccessors for temp, city and wind

--- lib/cli/weather.rb
+++ lib/cli/weather.rb
@@ -1,10 +1,21 @@
 class Weather::Weather
-  attr_accessor :data, :get_temperature
+  attr_accessor :data, :temp, :city, :wind
 
     def initialize(hash)
         @data = hash["data"][0]
     end
     
+    def temp
+      
+    end
+
+    def city
+      
+    end
+
+    def wind
+      
+    end
 end
 
 # @data["data"][0]["ob_time"] time and date
